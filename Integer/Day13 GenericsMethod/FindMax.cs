5711e058c089bccc9544564f162319e23c7ca7b9
srisri96 <srikanthsitharth80@gmil.com>
2022-07-22 18:31:28 +0530

    Integeradded

---
+++ Integer/Day13 GenericsMethod/FindMax.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day13_GenericsMethod
+{
+    internal class FindMax
+    {
+        public static void ToCompare(int a, int b, int c)
+        {
+            if (a.CompareTo(b) > 0 && a.CompareTo(c) > 0)
+            {
+                Console.WriteLine($"{a} is Maximum");
+            }
+            else if (b.CompareTo(a) > 0 && b.CompareTo(c) > 0)
+            {
+                Console.WriteLine($"{b} is Maximum");
+            }
+            else
+            {
+                Console.WriteLine($"{c} is Maximum");
+            }
+        }
+    }
+}
---
+++ Integer/Day13 GenericsMethod/Program.cs
@@ -0,0 +1,4 @@
+﻿using Day13_GenericsMethod;
+Console.WriteLine("Welcome to Generics program to find the maximum of three values");
+FindMax check = new FindMax();
+check.ToCompare(3, 2, 1);
