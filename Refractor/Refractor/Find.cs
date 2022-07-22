f4c490861ae6cd9e60fcc2c74035cc4ae4774c97
srisri96 <srikanthsitharth80@gmil.com>
2022-07-22 18:33:26 +0530

    Refractor

---
+++ Refractor/Refractor/Find.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Refractor
+{
+    internal class Find
+    {
+        public void ToCompare<T>(T a, T b, T c) where T : IComparable //Generic method
+        {
+            if (a.CompareTo(b) > 0 && a.CompareTo(c) > 0 ||
+                a.CompareTo(b) >= 0 && a.CompareTo(c) > 0 ||
+                a.CompareTo(b) > 0 && a.CompareTo(c) >= 0)
+            {
+                Console.WriteLine($"{a} is Maximum");
+            }
+            else if (b.CompareTo(a) > 0 && b.CompareTo(c) > 0 ||
+                b.CompareTo(a) >= 0 && b.CompareTo(c) > 0 ||
+                b.CompareTo(a) > 0 && b.CompareTo(c) >= 0)
+            {
+                Console.WriteLine($"{b} is Maximum");
+            }
+            else
+            {
+                Console.WriteLine($"{c} is Maximum");
+            }
+        }
+    }
+}
---
+++ Refractor/Refractor/Program.cs
@@ -0,0 +1,8 @@
+﻿using Refractor;
+Console.WriteLine("Welcome to Generics program to find the maximum of three values");
+Find check1 = new Find();
+Find check2 = new Find();
+Find check3 = new Find();
+check1.ToCompare(3, 2, 1);
+check2.ToCompare(1.234, 2.463, 1.534);
+check3.ToCompare("Sourava", "Srivatsan", "srinivasan");
