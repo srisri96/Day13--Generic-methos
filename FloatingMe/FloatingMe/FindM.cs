99400d8bea890731ad297258aafaec014a96da19
srisri96 <srikanthsitharth80@gmil.com>
2022-07-22 18:32:38 +0530

    Floating value

---
+++ FloatingMe/FloatingMe/FindM.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FloatingMe
+{
+    internal class FindM
+    {
+        public void ToCompare(float a, float b, float c)
+        {
+            if (a.CompareTo(b) > 0 && a.CompareTo(c) > 0)
+            {
+                Console.WriteLine($"{a} is Maximum");
+            }
+            else if (b.CompareTo(a) > 0 && b.CompareTo(c) > 0)
+            {
+                Console.WriteLine($"{b} is Maximum");
+            }
+            else
+            {
+                Console.WriteLine($"{c} is Maximum");
+            }
+        }
+    }
+}
---
+++ FloatingMe/FloatingMe/Program.cs
@@ -0,0 +1,4 @@
+﻿using FloatingMe;
+Console.WriteLine("Welcome to Generics program to find the maximum of three values");
+FindM check = new FindM();
+check.ToCompare(3.2f, 2.1f, 1.9f);
